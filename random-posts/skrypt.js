22d680588dce3359fc5fb2ce21192ed532f298ce
bloggerwidgets <77089636+bloggerwidgets@users.noreply.github.com>
2021-07-29 08:19:16 +0200

    Add files via upload

--- random-posts/skrypt.js
+++ random-posts/skrypt.js
@@ -391,7 +391,7 @@ function generujKod() {
 		kap = kap.substring(0, kap.length-1);
 		kod += ' hide="' + kap + '"'
 	}
-	kod += ' async src="https://cdn.jsdelivr.net/gh/bloggerwidgets/scripts@main/randomposts.js"><\/script>';
+	kod += ' async src="https://cdn.jsdelivr.net/gh/bloggerwidgets/scripts@1.1.5/randomposts.js"><\/script>';
 	return kod;
 }
 
